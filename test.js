c9e8e8bfd32104234436acf6a009b20cff9eaec5
Jason Laster <jason.laster.11@gmail.com>
2021-11-05 20:11:31 -0400

    new line (#1)

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
+const { chromium } = require("playwright");
 console.log(`>>> TEST (start)`);
 console.log(`>>> TEST (1)`, process.env.PLAYWRIGHT_TEST_BASE_URL);
+console.log(`>>> TEST (2)`);
+
 console.log(`>>> TEST (done)`);
+
+(async () => {
+  console.log("launching chromium");
+  const browser = await chromium.launch({ headless: true });
+  console.log("launched chromium");
+  const page = await browser.newPage();
+  await page.goto(process.env.PLAYWRIGHT_TEST_BASE_URL);
+  // other actions...
+  console.log("waiting for a");
+  await page.waitForSelector("a");
+  console.log("found a a");
+  await browser.close();
+})();
