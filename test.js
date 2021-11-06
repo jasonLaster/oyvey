2a410341508abaa941ee483caa5ec74e3a7c6cc5
Jason Laster <jason.laster.11@gmail.com>
2021-11-05 17:02:52 -0700

    Add playwright

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
+const { chromium } = require("playwright");
 console.log(`>>> TEST (start)`);
 console.log(`>>> TEST (1)`, process.env.PLAYWRIGHT_TEST_BASE_URL);
 console.log(`>>> TEST (2)`);
 
 console.log(`>>> TEST (done)`);
+
+(async () => {
+  console.log("launching chromium");
+  const browser = await chromium.launch(); // Or 'firefox' or 'webkit'.
+  console.log("launched chromium");
+  const page = await browser.newPage();
+  await page.goto(process.env.PLAYWRIGHT_TEST_BASE_URL);
+  // other actions...
+  console.log("waiting for a");
+  await page.waitForSelector("a");
+  console.log("found a a");
+  await browser.close();
+})();
